14e79b10731ddc1b486729af57177e824806fa34
Juan Ruiz <j.ruiz3750@gmail.com>
2015-10-31 15:39:11 -0700

    Started working on problem 6

---
+++ Problem 6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problem 6
+//
+//  Created by Juan on 10/31/15.
+//  Copyright © 2015 Juan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
