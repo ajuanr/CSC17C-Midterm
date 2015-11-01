b42a4da6ccba46cf9d08123981411154191f085d
Juan Ruiz <j.ruiz3750@gmail.com>
2015-10-31 21:46:57 -0700

    Working on problem 7

---
+++ Problem7/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  Problem7
+//
+//  Created by Juan on 10/31/15.
+//  Copyright © 2015 Juan. All rights reserved.
+//
+
+#include <iostream>
+
+#include "SelfOrganizingList.h"
+
+using namespace std;
+
+void fill(LnkdLst<int>&, int, int = 9);
+void printLst(LnkdLst<int>&, int=100);
+
+int main(int argc, const char * argv[]) {
+    LnkdLst<int> *ll = new SelfOrganizingList<int>();
+    
+    fill(*ll, 10, 9);
+    printLst(*ll);
+    //cout << ll->inList(7) << endl;
+    ll->insertBefore(0, 0);
+    printLst(*ll);
+    
+    return 0;
+}
+
+void fill(LnkdLst<int>& ll, int size, int mod) {
+    // use current time as seed for random number generator
+    srand(static_cast<unsigned int>(time(0)));
+    for (int i = 0; i != size; ++i) {
+        ll.append(rand() % mod + 1);
+    }
+}
+
+void printLst(LnkdLst<int>& ll, int p) {
+    for (int i = 0; i != ll.getSize(); ++i) {
+        cout << ll.get(i) << " ";
+        if (i % p == p-1)
+            cout << endl;
+    }
+    cout << endl;
+}
